9d7416ff618a59a05702929b192424694a1591e5
Reema Roy <reemaroy73@gmail.com>
2024-03-04 13:00:28 -0800

    Create instance of express and listen to port.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 console.log("hello");
+const express = require("express");
+const app=express();
+
+app.listen(8080, () =>{
+    console.log("Server is now listening at http://localhost:8080 ");
+})
