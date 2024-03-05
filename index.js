97862dd58982cd0e675e0c738b4e131c7f726ab6
Reema Roy <reemaroy73@gmail.com>
2024-03-04 16:13:45 -0800

    Implement Get, Post methods for API.

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-console.log("hello");
 const express = require("express");
+const videoRoute = require('./routes/videos') ;
 const app=express();
+
+app.use(express.static("public"));
+app.use(express.json());
+
+// app.use((req, res, next) => {
+//     console.log(`Incoming request: ${req.method} ${req.url}`);
+
+//     next();
+// })
+
+app.use('/videos', videoRoute);
 
 app.listen(8080, () =>{
     console.log("Server is now listening at http://localhost:8080 ");
---
+++ routes/videos.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const router = express.Router();
+const fs = require('fs');
+const crypto = require('crypto');
+
+const json_file_path = './data/videos.json';
+
+const getVideos = () => {
+    const videosJson = fs.readFileSync(json_file_path);
+    const videosData = JSON.parse(videosJson)
+    return videosData;
+}
+
+const setVideos = (video) => {
+    const videosJson = JSON.stringify(video)
+    fs.writeFileSync(json_file_path, videosJson);
+    console.log(json_file_path);
+}
+
+router
+    .route("/")
+    .get((req, res) => {
+        const videoData = getVideos();
+        const videoFilteredList = videoData.map((video) => ({
+            id: video.id,
+            title: video.title,
+            channel: video.channel,
+            image: video.image,
+        }))
+        res.send(videoFilteredList)
+    })
+
+router
+    .route("/:videoId")
+    .get((req, res) => {
+        const videoId = (req.params.videoId);
+
+        const videoData = getVideos();
+
+        const clickedVideoDetails = videoData.find((data) => {
+            return data.id === videoId;
+        })
+        res.send(clickedVideoDetails)
+    })
+
+router
+    .route("/")
+    .post((req, res) => {
+        console.log(req.body)
+        const { title, description } = req.body;
+
+        const videoData = getVideos();
+
+        const newVideo = {
+            id: crypto.randomUUID(),
+            title: title,
+            channel: "Reema Roy",
+            image: './images/new-video.jpg',
+            description: description,
+            views: 0,
+            likes: 0,
+            duration: "45:59",
+            video: "https://unit-3-project-api-0a5620414506.herokuapp.com/stream",
+            timestamp: new Date,
+            comments: []
+        };
+        videoData.push(newVideo);
+        setVideos(videoData);
+        res.send(newVideo);
+    })
+
+module.exports = router;
